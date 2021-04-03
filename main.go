1a97c3ddd0cffc1908ce03cc4f818ef604e80be0
Koichi Shiraishi <zchee.io@gmail.com>
2021-04-03 23:49:05 +0900

    main,deepcopy: initial implementation

---
+++ main.go
@@ -0,0 +1,43 @@
+// SPDX-FileCopyrightText: Copyright 2021 The protobuf-tools Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Command protoc-gen-deepcopy is a plugin for protoc which generates DeepCopyInto(), DeepCopy() and DeepCopyInterface() functions for .pb.go types.
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"google.golang.org/protobuf/compiler/protogen"
+
+	"github.com/protobuf-tools/protoc-gen-deepcopy/deepcopy"
+)
+
+// version is the this binary vesion.
+// TODO(zchee): parse module version itself.
+var version = "v0.0.0"
+
+func main() {
+	showVersion := flag.Bool("version", false, "print the version and exit")
+	flag.Parse()
+	if *showVersion {
+		fmt.Printf("protoc-gen-go-grpc %v\n", version)
+		return
+	}
+
+	var flags flag.FlagSet
+
+	protogen.Options{
+		ParamFunc: flags.Set,
+	}.Run(func(gen *protogen.Plugin) error {
+		gen.SupportedFeatures = deepcopy.SupportedFeatures
+
+		for _, f := range gen.Files {
+			if f.Generate {
+				deepcopy.GenerateFile(gen, f)
+			}
+		}
+
+		return nil
+	})
+}
